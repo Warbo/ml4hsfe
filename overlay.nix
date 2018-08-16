e1e947037bfa995c4c3679c4b0769725ca3c5c92
Chris Warburton <chriswarbo@gmail.com>
2018-08-16 02:24:57 +0100

    Move definitions from release into overlay

---
+++ overlay.nix
@@ -0,0 +1,77 @@
+self: super:
+
+with builtins;
+with {
+  mkHs = { profile ? false }: self.haskell.packages.ghc7103.override (old: {
+    overrides = self.lib.composeExtensions
+      (old.overrides or (_: _: {}))
+      (helf: huper: {
+        # Toggle profiling support
+        mkDerivation = if profile
+                          then (args: huper.mkDerivation (args // {
+                                 enableLibraryProfiling = true;
+                               }))
+                          else huper.mkDerivation;
+
+        # Hackage dependencies
+
+        # Must be < 2.10 and 2.9 causes conflicts elsewhere
+        QuickCheck = helf.callHackage "QuickCheck" "2.8.2" {};
+
+        # This must be < 2
+        quickspec  = helf.callHackage "quickspec"  "0.9.6" {};
+
+        # Non-Hackage dependencies
+
+        HS2AST  = helf.callPackage
+          (helf.haskellSrc2nix {
+            name = "HS2AST";
+            src  = self.fetchgit {
+              url    = http://chriswarbo.net/git/hs2ast.git;
+              rev    = "f48063e";
+              sha256 = "1jg62a71mlnm0k2sjbjhf3n5q2c4snlbaj5dlrhdg44kxiyilx9x";
+            };
+          }) {};
+
+        # Nix overrides
+
+        # Test suite dependencies clash
+        aeson = super.haskell.lib.dontCheck huper.aeson;
+
+        # This requires semigroups if GHC < 8, but was generated using GHC 8
+        system-filepath = super.haskell.lib.addBuildDepend huper.system-filepath
+                                                           helf.semigroups;
+
+        # Ourselves
+
+        ML4HSFE = helf.callPackage
+          (helf.haskellSrc2nix {
+            name = "ML4HSFE";
+            src  = filterSource
+              (path: _:
+                with rec {
+                  unwanted = [ ".git" "asv.conf.json" "benchmarks" "dist"
+                               "dist-newstyle" "result" ];
+                  isNix    = self.lib.hasSuffix ".nix" path;
+                };
+                !(elem (baseNameOf path) unwanted || isNix))
+              ./.;
+          }) {};
+    });
+  });
+};
+{
+  helpersSrc = self.fetchgit {
+    url    = http://chriswarbo.net/git/nix-helpers.git;
+    rev    = "ed8379a";
+    sha256 = "1ifyz49x9ck3wkfw3r3yy8s0vcknz937bh00033zy6r1a2alg54g";
+  };
+
+  hsPkgs = mkHs {};
+
+
+  inherit (self.hsPkgs) ML4HSFE;
+
+  # Force system's Nix, to ensure compatibility with whatever's running us
+  inherit (import <nixpkgs> {}) nix;
+}
--- release.nix
+++ release.nix
@@ -1,69 +1,13 @@
 with rec {
-  sysPkgs = import <nixpkgs> {};
-  helpers = sysPkgs.fetchgit {
-    url    = http://chriswarbo.net/git/nix-helpers.git;
-    rev    = "ed8379a";
-    sha256 = "1ifyz49x9ck3wkfw3r3yy8s0vcknz937bh00033zy6r1a2alg54g";
-  };
-  mkPkgs = path: import path {
-    overlays = [
-      # Load our helper functions
-      (import "${helpers}/overlay.nix")
+  sysPkgs = import <nixpkgs> { overlays = [ (import ./overlay.nix) ]; };
 
-      # Force system's Nix, to ensure compatibility with whatever's running us
-      (self: super: { inherit (sysPkgs) nix; })
-    ];
-  };
-};
+  helpers = import sysPkgs.helpersSrc;
 
-# Load a pinned version of nixpkgs (except for 'nix') with our helpers available
-with mkPkgs (mkPkgs <nixpkgs>).repo1803;
-with builtins;
-with rec {
-  src = filterSource (path: _: !(elem (baseNameOf path) [
-                       ".git" "dist" "dist-newstyle"
-                     ]))
-                     ./.;
-
-  HS2AST = latestGit {
-    url = http://chriswarbo.net/git/hs2ast.git;
-  };
-
-  hsPkgs = { extra ? (_: _: {}) }: haskell.packages.ghc7103.override (old: {
-    overrides = self: super: extra self super // {
-      # Hackage dependencies
-
-      # Must be < 2.10 and 2.9 causes conflicts elsewhere
-      QuickCheck = self.callHackage "QuickCheck" "2.8.2" {};
-
-      # This must be < 2
-      quickspec  = self.callHackage "quickspec"  "0.9.6" {};
-
-      # Non-Hackage dependencies
-
-      HS2AST  = self.callPackage (self.haskellSrc2nix {
-                                   name = "HS2AST";
-                                   src  = HS2AST;
-                                 }) {};
-
-      # Nix overrides
-
-      # Test suite dependencies clash
-      aeson = haskell.lib.dontCheck super.aeson;
-
-      # This requires semigroups if GHC < 8, but was generated using GHC 8
-      system-filepath = haskell.lib.addBuildDepend super.system-filepath
-                                                   self.semigroups;
-
-      # Ourselves
-
-      ML4HSFE = self.callPackage (self.haskellSrc2nix {
-                                   inherit src;
-                                   name = "ML4HSFE";
-                                 }) {};
-    };
-  });
+  pinned  = import helpers.repo1803 { overlays = [
+    (import "${sysPkgs.helpersSrc}/overlay.nix")
+    (import ./overlay.nix)
+  ]; };
 };
 {
-  package = (hsPkgs {}).ML4HSFE;
+  inherit (pinned) ML4HSFE;
 }
