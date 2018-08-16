c01b6935d121f54333687600a5291e67cf7d6ea8
Chris Warburton <chriswarbo@gmail.com>
2018-08-16 13:33:25 +0100

    Force ASTs to normal form when parsing

--- src/Types.hs
+++ src/Types.hs
@@ -2,13 +2,20 @@
 
 module Types where
 
+import           Control.DeepSeq (($!!), deepseq, force, NFData, rnf)
 import           Control.Monad
 import           Data.Aeson
 import           HS2AST.Types
 
-data ASTId = ASTId { aId :: Identifier
-                   , aDeps :: [Identifier]
+data ASTId = ASTId { aId   :: !Identifier
+                   , aDeps :: ![Identifier]
                    } deriving (Show, Eq)
+
+instance NFData ASTId where
+  rnf (ASTId id deps) = id `deepseq` deps `deepseq` ()
+
+instance NFData Identifier where
+  rnf i = idName i `deepseq` idPackage i `deepseq` idModule i `deepseq` ()
 
 aName    = idName    . aId
 aPackage = idPackage . aId
@@ -29,7 +36,7 @@ instance FromJSON ASTId where
     p <- x .: "package"
     m <- x .: "module"
     d <- x .: "dependencies"
-    return ASTId {
+    return $!! ASTId {
         aId = ID {
             idName    = n
           , idPackage = p
