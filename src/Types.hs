4d8c2adb67bcee6b8b6432d8cad5d435e838d285
Chris Warburton <chriswarbo@gmail.com>
2018-08-17 17:09:41 +0100

    Pull out parser

--- src/Types.hs
+++ src/Types.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE BangPatterns      #-}
 {-# LANGUAGE OverloadedStrings #-}
 
 module Types where
@@ -5,6 +6,8 @@ module Types where
 import           Control.DeepSeq (($!!), deepseq, force, NFData, rnf)
 import           Control.Monad
 import           Data.Aeson
+import qualified Data.HashMap.Strict as HM
+import qualified Data.Maybe          as M
 import           HS2AST.Types
 
 data ASTId = ASTId { aId   :: !Identifier
@@ -31,17 +34,17 @@ instance ToJSON ASTId where
                    ]
 
 instance FromJSON ASTId where
-  parseJSON (Object x) = do
-    n <- x .: "name"
-    p <- x .: "package"
-    m <- x .: "module"
-    d <- x .: "dependencies"
-    return $!! ASTId {
-        aId = ID {
-            idName    = n
-          , idPackage = p
-          , idModule  = m
-          }
-      , aDeps = d
-      }
-  parseJSON _ = mzero
+  parseJSON x = pure (valToAstId x)
+
+valToAstId (Object x) = ASTId {
+                          aId = ID {
+                                  idName    = n,
+                                  idPackage = p,
+                                  idModule  = m
+                                },
+                          aDeps = d
+                        }
+  where Just (String !n) = HM.lookup "name"         x
+        Just (String !p) = HM.lookup "package"      x
+        Just (String !m) = HM.lookup "module"       x
+        Success      !d  = fromJSON (M.fromJust (HM.lookup "dependencies" x))
